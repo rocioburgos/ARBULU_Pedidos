3b6b3c8bdcf1033cce523aff635b9a1448189b62
Rocio <rocioburgos00@gmail.com>
2022-10-18 20:10:18 -0300

    first commit

---
+++ ARBULU_Pedidos/android/app/src/main/java/io/arbulu/pedidos/MainActivity.java
@@ -0,0 +1,5 @@
+package io.arbulu.pedidos;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ ARBULU_Pedidos/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.arbulu.pedidos',
+  appName: 'ARBULU_Pedidos',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
